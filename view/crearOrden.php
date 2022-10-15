47c5cd785eb1a312fe843cc5f7b15b25d43eea0c
nicfer21 <ferboncun@gmail.com>
2022-10-15 01:18:12 -0500

    proyecto

--- view/crearOrden.php
+++ view/crearOrden.php
@@ -16,7 +16,7 @@
           <!-- Default box -->
           <div class="card">
               <div class="card-header">
-                  <h3 class="card-title">Primer avance</h3>
+                  <h3 class="card-title">Creacion de ordenes de trabajo</h3>
 
                   <div class="card-tools">
                       <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
@@ -27,17 +27,8 @@
               </div>
               <div class="card-body">
                   <div class="row">
-                      <div class="col-lg-6 col-sm-12">
-                          <!-- Main content 
-              <h5>Excel de diagnostico inicial</h5>
-              <div id="myExcelDiv1" style="width: 500px; height: 300px"></div>
-              <script type="text/javascript" src="https://onedrive.live.com/embed?resid=2EDA39EC46A50B0F%211769&authkey=%21ALPK_3hU2BqVR0M&em=3&wdDivId=%22myExcelDiv1%22&wdDownloadButton=1&wdHideGridlines=1&wdHideHeaders=1&wdAllowInteractivity=0"></script>
-            </div>
-            <div class="col-lg-6 col-sm-12">
-              <h5>Informe del diagnostico inicial</h5>
-              <iframe src="https://onedrive.live.com/embed?cid=2EDA39EC46A50B0F&amp;resid=2EDA39EC46A50B0F%211770&amp;authkey=ACPIut3u1MxbFwY&amp;em=2" width="500px" height="300px" frameborder="0">Esto es un documento de <a target="_blank" href="https://office.com">Microsoft Office</a> incrustado con tecnología de <a target="_blank" href="https://office.com/webapps">Office</a>.</iframe>
-            </div>
-            -->
+                      <div class="col-lg-12 col-sm-12">
+                            
                       </div>
                   </div>
                   <!-- /.card-body -->
@@ -52,3 +43,5 @@
       <!-- /.content -->
   </div>
   <!-- /.content-wrapper -->
+
+  <script src="dist/js/crearOrden.js"></script>
