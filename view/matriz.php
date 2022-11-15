c81bdb2145785db1c162efca78c8e9fc2fa8ce03
nicfer21 <ferboncun@gmail.com>
2022-11-15 17:56:12 -0500

    Proyecto

--- view/matriz.php
+++ view/matriz.php
@@ -66,9 +66,56 @@
               </div>
               <div class="card-body">
                   <div class="row">
-                      <div class="col-lg-6 col-sm-12">
+                      <div class="col-12">
+                          <h5>Excel de planeacion</h5>
+                          <iframe width="700" height="400" frameborder="0" scrolling="no" src="https://onedrive.live.com/embed?resid=2EDA39EC46A50B0F%211780&authkey=%21AIWLXy5UxquorAY&em=2&wdAllowInteractivity=False&wdHideGridlines=True&wdHideHeaders=True&wdDownloadButton=True&wdInConfigurator=True&wdInConfigurator=True&edesNext=true&resen=false"></iframe>
+                          <br>
+                          <hr>
+                          <a class="btn btn-link" href="https://1drv.ms/x/s!Ag8LpUbsOdoujXT7HjQGDUiKvAiS?e=m1u8YT" target="_blank" rel="noopener noreferrer">Descargar excel de la planeacion <i class="fa fa-download" aria-hidden="true"></i></a>
+                          <br>
+                      </div>
+
+                      <hr>
+
+                      <div class="col-12">
+                          <br>
+                          <h5>Informe de planeacion</h5>
+                          <iframe src="https://onedrive.live.com/embed?cid=2EDA39EC46A50B0F&amp;resid=2EDA39EC46A50B0F%211779&amp;authkey=ALVT5letQUg9zIY&amp;em=2" width="700px" height="400px" frameborder="0">Esto es un documento de <a target="_blank" href="https://office.com">Microsoft Office</a> incrustado con tecnología de <a target="_blank" href="https://office.com/webapps">Office</a>.</iframe>
+                          <br>
+                          <hr>
+                          <a class="btn btn-link" href="https://1drv.ms/w/s!Ag8LpUbsOdoujXO2W_bl1qC01pxh?e=SuQmli" target="_blank" rel="noopener noreferrer">Descargar word de la planeacion <i class="fa fa-download" aria-hidden="true"></i></a>
+                      </div>
+
+                  </div>
+                  <!-- /.card-body -->
+
+              </div>
+              <!-- /.card -->
+          </div>
+
+          <div class="card">
+              <div class="card-header">
+                  <h3 class="card-title">MATRIZ DE TRABAJO</h3>
+
+                  <div class="card-tools">
+                      <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+                          <i class="fas fa-minus"></i>
+                      </button>
+                  </div>
+
+              </div>
+              <div class="card-body">
+                  <div class="row">
+                      <div class="col-12">
 
                       </div>
+
+                      <hr>
+
+                      <div class="col-12">
+
+                      </div>
+
                   </div>
                   <!-- /.card-body -->
 
