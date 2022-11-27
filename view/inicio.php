290fbe674001174a570944549cfc230b064dd6fc
nicfer21 <ferboncun@gmail.com>
2022-11-26 23:23:00 -0500

    Proyecto

--- view/inicio.php
+++ view/inicio.php
@@ -27,7 +27,7 @@
             <div class="icon">
               <i class="ion ion-bag"></i>
             </div>
-            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            
           </div>
         </div>
 
@@ -43,7 +43,7 @@
             <div class="icon">
               <i class="ion ion-stats-bars"></i>
             </div>
-            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            
           </div>
         </div>
 
@@ -59,7 +59,7 @@
             <div class="icon">
               <i class="ion ion-person-add"></i>
             </div>
-            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            
           </div>
         </div>
         <!-- ./col -->
@@ -75,15 +75,25 @@
             <div class="icon">
               <i class="ion ion-pie-graph"></i>
             </div>
-            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            
           </div>
         </div>
         <!-- ./col -->
 
+
+
+
+
+        <!-- Graficos de datos -->
         <div class="col-lg-6 col-sm-12">
           <div class="card card-primary">
             <div class="card-header">
-              <h3 class="card-title">Grafico 1</h3>
+              <h3 class="card-title">Tipos de mantenimiento - Mes : <?php
+                date_default_timezone_set('America/Lima');
+                echo date('l jS \of F Y h:i:s A');
+                //echo date('D F Y');
+              
+              ?></h3>
 
             </div>
             <!-- /.card-header -->
